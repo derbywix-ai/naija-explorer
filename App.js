8fe909edbc8c0e5041fab1eeafd998c7d030158a
derbywix-ai <derbywix@gmail.com>
2025-11-07 17:57:24 +0100

    project

--- App.js
+++ App.js
@@ -4,6 +4,11 @@ import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
+import CityScreen from "./screens/CityScreen";
+import HotelScreen from "./screens/HotelScreen";
+import EventScreen from "./screens/EventScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 const Stack = createStackNavigator();
 
@@ -14,6 +19,11 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="City" component={CityScreen} />
+        <Stack.Screen name="Hotel" component={HotelScreen} />
+        <Stack.Screen name="Event" component={EventScreen} />
+        <Stack.Screen name="Favorites" component={FavoritesScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
